441db93ad023e4e3d2d3c715c44352fa5e7dadd7
Reginald Costa <reginald.costa@gmail.com>
2017-09-27 14:25:03 +0200

    jour1PM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-//import React from 'react'
-import GifLoader from "./GifLoader";
+//import GifLoader from "./GifLoader";
+//GifLoader.loadGif("cat").then(gif => console.log(gif));
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-GifLoader.loadGif("cat").then(gif => console.log(gif));
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('app'));
